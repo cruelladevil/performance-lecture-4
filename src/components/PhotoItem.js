e9564d18c95d7c5ca5a8df9f8f1fe82658082bab
cruelladevil <dev.timetravel@gmail.com>
2023-09-05 04:28:00 +0900

    feat: 작은 이미지로 배경 평균 계산

--- src/components/PhotoItem.js
+++ src/components/PhotoItem.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
-import { showModal } from '../redux/imageModal';
+import { setBgColor, showModal } from '../redux/imageModal';
 import LazyLoad from 'react-lazyload';
+import { getAverageColorOfImage } from '../utils/getAverageColorOfImage';
 
 function PhotoItem({ photo: { urls, alt } }) {
   const dispatch = useDispatch();
 
-  const openModal = () => {
+  const openModal = event => {
     dispatch(showModal({ src: urls.full, alt }));
+
+    const averageColor = getAverageColorOfImage(event.target);
+    dispatch(setBgColor(averageColor));
   };
 
   return (
     <LazyLoad height={200} offset={1000}>
-      <Image src={urls.small + '&t=' + new Date().getTime()} alt={alt} onClick={openModal} />
+      <Image
+        src={urls.small + '&t=' + new Date().getTime()}
+        alt={alt}
+        onClick={openModal}
+        crossOrigin="*"
+      />
     </LazyLoad>
   );
 }
